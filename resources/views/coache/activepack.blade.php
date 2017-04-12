97e6c771251353222e293e88d535f2d8b8c43214
Kazim Raza <kazim.friendly@gmail.com>
2017-04-12 20:15:55 +0500

    UI applied to Active Packages for Admin and Coaches.

--- resources/views/coache/activepack.blade.php
+++ resources/views/coache/activepack.blade.php
@@ -25,7 +25,7 @@
                     <table class="table">
                         <thead>
                             <tr>
-                                <th style="width: 40px;"></th>
+                                <!--<th style="width: 40px;"></th>-->
                                 <th>Name </th>
                                 <th>Status </th>
                                 <th>Action</th>
@@ -36,11 +36,10 @@
                             @foreach ($assignments as $assigned)
                             @foreach ($assigned->package()->get() as $package)
                             <tr id="package_{{$package->id}}">
-                                <td>+</td>
+                                <!--<td>+</td>-->
                                 <td colspan="2">{{$package->title}}</td>
 
-                                <td><button class="btn btn-success viewmodules" value="{{$package->id}}" title="Show Modules"><i class="fa fa-arrow-circle-o-down" ></i></button></td>
-
+                                <td><button class="btn btn-success viewmodules" value="{{$package->id}}" title="Show Modules"><i class="fa fa-arrow-circle-o-down" ></i> Show Modules</button></td>
                             </tr>
 
                             @foreach($package->selected_modules as $module)
@@ -51,26 +50,88 @@
                             ->where('module_id',$module->id);
                             @endphp
 
+                            
+                            @if($loop->index == 0)
+                            <tr id="packmodule_{{$package->id}}" class="package_module"><td colspan="3">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title">Selected Package Modules</h3>
+                                        </div>
+                                        <div class="panel-body">                    
 
-                            <tr colspan="3" class="warning" id="packmodule_{{$package->id}}">
-                                <td style="width: 40px;">--</td>
-                                <td colspan="2">{{$module->title}}</td> 
+                                            <table class="table">
+                                                <thead>
+                                                    <tr>
+                                                        <!--<th style="width: 40px;"></th>-->
+                                                        <th>Name </th>
+                                                        <th>Description</th>
+                                                        <th width="50">Action</th>                
+                                                    </tr>
+                                                </thead>
+                                                <tbody id="ass-pack-list" name="ass-pack-list">                            
+                                                    @endif
+
+                            
+
+                            <tr colspan="3" class="info">
+                                <!--<td style="width: 40px;">--</td>-->
+                                <td>{{$module->title}}</td>
+                                <td>{{$module->description}}</td>
                                 @if($clients->count() > 0)
-                                <td><button class="btn btn-warning viewclients" value="{{$package->id}}-{{$module->id}}" title="Show Clients"><i class="fa fa-arrow-circle-o-down" ></i></button></td>
+                                <td><button class="btn btn-warning viewclients" value="{{$package->id}}-{{$module->id}}" title="Show Clients"><i class="fa fa-caret-square-o-down" ></i> Show Client</button></td>
                                 @else
-                                <td><button class="btn btn-warning viewclients disabled" value="{{$package->id}}-{{$module->id}}" title="Show Clients"><i class="fa fa-arrow-circle-o-down" ></i></button></td>
+                                <td><button class="btn btn-warning viewclients disabled" value="{{$package->id}}-{{$module->id}}" title="Show Clients"><i class="fa fa-caret-square-o-down" ></i> Show Client</button></td>
                                 @endif
                             </tr>
 
                             @foreach($clients as $client)
+
+                            @if($loop->index == 0)
+                            <tr id="packclient_{{$package->id}}-{{$module->id}}" class="package_module"><td colspan="3">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title">Selected Module Clients</h3>
+                                        </div>
+                                        <div class="panel-body">                    
+
+                                            <table class="table">
+                                                <thead>
+                                                    <tr>
+                                                        <!--<th style="width: 40px;"></th>-->
+                                                        <th>Name </th>
+                                                        <th>Status</th>
+                                                        <th width="50">Action</th>                
+                                                    </tr>
+                                                </thead>
+                                                <tbody id="ass-pack-list" name="ass-pack-list">                            
+                                                    @endif
+
                             <tr colspan="3" class="success" id="packclient_{{$package->id}}-{{$module->id}}">
-                                <td style="width: 40px;">-></td>
+                                <!--<td style="width: 40px;">-></td>-->
                                 <td>{{$client->user->name}}</td>
                                 <td>{{$client->getStatus()}}</td>
-                                <td><a class="btn btn-info btn-detail preview_module" href="{{ url('assigned/'.$client->id) }}" title="Preview"><i class="fa fa-search" ></i></a></td>
-
+                                <td><a class="btn btn-info btn-detail preview_module" href="{{ url('assigned/'.$client->id) }}" title="Preview"><i class="fa fa-search" ></i> View Module</a></td>
                             </tr>
+
+                                                   @if($loop->count+1 == $loop->last)
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                </td>
+                            </tr>
+                            @endif  
+
                             @endforeach
+
+
+                                                   @if($loop->count+1 == $loop->last)
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                </td>
+                            </tr>
+                            @endif  
+
                             @endforeach
 
                             @endforeach
@@ -83,7 +144,7 @@
                     <table class="table" id="activeClientstbl">
                         <thead>
                             <tr>
-    <!--                                        <th>ID</th>-->
+                                <!--<th>ID</th>-->
                                 <th>Name</th>
                                 <th>Status</th>
                                 <th>Actions</th>
@@ -91,16 +152,70 @@
                         </thead>
                         <tbody id="client_active" name="client_active">
                             @foreach($collection->whereIn('coache_id',$assignments->pluck('id'))->unique('user_id') as $client)
+                            @php
+                            $pack_id = $package->id;
+                            @endphp
                             <tr>
-                                <td colspan="3">{{$client->user->name}} </td>
+                                <td colspan="2">{{$client->user->name}} </td>
+                                <td><button class="btn btn-success view_assigned_packages" value="{{$client->user->id.'_'.$pack_id}}" title="Show Packages"><i class="fa fa-caret-square-o-down" ></i> Assigned Packages</button></td> <!--$package->id-->
                             </tr>
+
                             @foreach($client->getPackages(null,$collection) as $package)
+                            @if($loop->index == 0)
+                            <tr id="assignedpack_{{$client->user->id.'_'.$pack_id}}" class="active_package"><td colspan="3">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title">Selected Client Packages</h3>
+                                        </div>
+                                        <div class="panel-body">                    
+
+                                            <table class="table">
+                                                <thead>
+                                                    <tr>
+                                                        <!--<th style="width: 40px;"></th>-->
+                                                        <th>Name </th>
+                                                        <th>Description</th>
+                                                        <th>Status</th>
+                                                        <th width="50">Action</th>                
+                                                    </tr>
+                                                </thead>
+                                                <tbody id="ass-pack-list" name="ass-pack-list">                            
+                                                    @endif
+
+
                             <tr class="info">
-                                <td colspan="3">{{$package->title}} </td>
+                                <td>{{$package->title}} </td>
+                                <td>{{$package->description}} </td>
+                                <td>&nbsp;</td>     
+                                <td><button class="btn btn-success viewpackagemodules" value="{{$client->user->id.'_'.$package->id}}" title="Show Packages"><i class="fa fa-caret-square-o-down" ></i> Assigned Modules</button></td>
                             </tr>   
                             @foreach($package->selected_modules as $module)
+
+                            @if($loop->index == 0)
+                            <tr id="pack_modules_{{$client->user->id.'_'.$package->id}}" class="active_modules"><td colspan="4">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title">Selected Client Package Modules</h3>
+                                        </div>
+                                        <div class="panel-body">                  
+
+                                            <table class="table">
+                                                <thead>
+                                                    <tr>
+                                                        <!--<th style="width: 40px;"></th>-->
+                                                        <th>Name </th>
+                                                        <th>Description</th>
+                                                        <th>Status</th>
+                                                        <th width="50">Action</th>                
+                                                    </tr>
+                                                </thead>
+                                                <tbody>                            
+                                                    @endif
+
+
                             <tr class="success">
                                 <td>{{$module->title}} </td>
+                                <td>{{$module->description}} </td>
                                 @php
                                 $activeModule=$collection->where('module_id',$module->id)->where('package_id',$package->id)->where('user_id',$client->user_id)->first();
                                 $isActive=($activeModule == null)?false:true;
@@ -133,11 +248,28 @@
                                 </td>
                                 <td>
                                     @if($isActive)
-                                    <a class="btn btn-info btn-detail preview_module" href="{{ url('assigned/'.$activeModule->id) }}" title="Preview"><i class="fa fa-search" ></i></a></td>
+                                    <a class="btn btn-info btn-detail preview_module" href="{{ url('assigned/'.$activeModule->id) }}" title="Preview"><i class="fa fa-search" ></i> View Module</a></td>
                                 @endif
                             </tr>   
+
+                                                   @if($loop->count+1 == $loop->last)
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                </td>
+                            </tr>
+                            @endif    
                             @endforeach
+
+                                                   @if($loop->count+1 == $loop->last)
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                </td>
+                            </tr>
+                            @endif                            
                             @endforeach
+
                             @endforeach
                         </tbody>
                     </table>
@@ -182,6 +314,11 @@ Coach
         console.log("ready!");
         $('[id^=packmodule_]').hide();
         $('[id^=packclient_]').hide();
+
+
+        $('[id^=assignedpack_]').hide();
+        $('[id^=pack_modules_]').hide();
+
         $('.detail div').hide();
         $('.order_by_client').show();
         $('.btn-group .btn:first').addClass("btn-primary");
@@ -189,6 +326,7 @@ Coach
     $(document).on('click', '.viewmodules', function (e) {
         var package_id = $(this).val();
         $('[id^=packmodule_' + package_id + ']').toggle();
+        $('[id^=packmodule_' + package_id + '] div').toggle();
         console.log(package_id);
     });
     $(document).on('click', '.viewclients', function (e) {
@@ -196,6 +334,19 @@ Coach
         $('[id^=packclient_' + package_id + ']').toggle();
     });
 
+    $(document).on('click', '.view_assigned_packages', function (e) {
+        var user_packageid = $(this).val();
+        $('[id^=assignedpack_' + user_packageid + ']').toggle();
+        $('[id^=assignedpack_' + user_packageid + '] div').toggle();
+    });
+
+    $(document).on('click', '.viewpackagemodules', function (e) {
+        var user_packageid = $(this).val();
+        $('[id^=pack_modules_' + user_packageid + ']').toggle();
+
+    });
+    
+    
     $('.btn-group .btn').on('click', function () { // On the click event for each button
         $('.btn-group .btn').removeClass('btn-primary');
         $(this) // get the button being clicked
@@ -203,6 +354,13 @@ Coach
 //    .siblings('.btn-primary') // get all sibling buttons which may already be selected
 //    .removeClass('btn-primary'); // remove the selected class
 //        alert($(this).val());
+
+        $('[id^=packmodule_]').hide();
+        $('[id^=packclient_]').hide();
+
+        $('[id^=assignedpack_]').hide();
+        $('[id^=pack_modules_]').hide();
+
         $('.detail div').hide();
         $('.order_' + $(this).val()).show();
     });
