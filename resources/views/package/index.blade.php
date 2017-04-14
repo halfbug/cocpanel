374d9ae020d12de1402b0f5865b3df290025fe4b
Kazim Raza <kazim.friendly@gmail.com>
2017-04-14 17:28:17 +0500

    Bug fixed for package id in package controller. Now Add New User and Existing User will open with empty values and not with the previous values.

--- resources/views/package/index.blade.php
+++ resources/views/package/index.blade.php
@@ -25,27 +25,26 @@
                     </thead>
                     <tbody id="package-list" name="package-list">
                         @foreach ($packages as $package)
-                            <?php //dd($package->linked_clients); ?>
-                        <tr id="package_{{$package->id}}">
+                        <tr id="package_{{$package->package_id}}">
                             <td>{{$package->title}}</td>
                             <td>{{$package->price}}</td>
-                            <td id="clients_{{$package->id}}">{{$package->linked_clients->unique('user_id')->count() }}</td>
+                            <td id="clients_{{$package->package_id}}">{{$package->linked_clients->unique('user_id')->count() }}</td>
 
                             <td>
                                 <div class="dropup">
-                                    <button class="btn btn-danger btn-detail dropdown-toggle pull-left dropdownMenu1" value="{{$package->id}}"  type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" title="Add Client"><i class="fa fa-user" ></i>
+                                    <button class="btn btn-danger btn-detail dropdown-toggle pull-left dropdownMenu1" value="{{$package->package_id}}"  type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" title="Add Client"><i class="fa fa-user" ></i>
                                     <span class="caret"></span>
                                     </button>
                                     <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                                        <li ><a class="add_client" data-value="{{$package->id}}" href="#">Add Existing Client</a></li>
+                                        <li ><a class="add_client" data-value="{{$package->package_id}}" href="#">Add Existing Client</a></li>
                                         <li role="separator" class="divider"></li>
-                                        <li><a class="new_client" data-value="{{$package->id}}" href="#">Add New Client</a></li>
+                                        <li><a class="new_client" data-value="{{$package->package_id}}" href="#">Add New Client</a></li>
                                     </ul>
                                 </div>&nbsp;
-                                <button class="btn btn-secondary btn-detail edit_package" value="{{$package->id}}" title="Edit"><i class="fa fa-edit" ></i></button>
-                                <button class="btn btn-warning linked_client" value="{{$package->id}}"  title="Linked Client"><i class="fa fa-group"></i></button>
-                                <button class="btn btn-success preview_package" value="{{$package->id}}" title="Preview"><i class="fa fa-search" ></i></button>
-                                <button class="btn btn-primary btn-delete copy_package" value="{{$package->id}}" title="Copy"><i class="fa fa-copy" ></i></button>
+                                <button class="btn btn-secondary btn-detail edit_package" value="{{$package->package_id}}" title="Edit"><i class="fa fa-edit" ></i></button>
+                                <button class="btn btn-warning linked_client" value="{{$package->package_id}}"  title="Linked Client"><i class="fa fa-group"></i></button>
+                                <button class="btn btn-success preview_package" value="{{$package->package_id}}" title="Preview"><i class="fa fa-search" ></i></button>
+                                <button class="btn btn-primary btn-delete copy_package" value="{{$package->package_id}}" title="Copy"><i class="fa fa-copy" ></i></button>
                             </td>
                         </tr>
                         @endforeach
