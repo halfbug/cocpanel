d57db5350cfabc4025c244d9b33a786469a573ff
Kazim Raza <kazim.friendly@gmail.com>
2017-05-06 16:34:58 +0500

    hide submit to client/coach button, if client reply off is checked.

--- app/Policies/AssignmentPolicy.php
+++ app/Policies/AssignmentPolicy.php
@@ -22,25 +22,24 @@ class AssignmentPolicy
     
     public function sendcoachAlert(User $user , assignment $assignment)
     {
-       /*if($assignment->package()->first()->status == 0)
+       if($assignment->package()->first()->status == 0)
             $clientReply=true;
         else
-            $clientReply=false;*/
-//        var_dump; 
-       if(\Auth::user()->isCoach() || \Auth::user()->isAdmin()){
-           //if($clientReply) //&& ($assignment->coach->id == \Auth::user()->id) 
+            $clientReply=false;
+//        var_dump;
+
+        if($clientReply && ($assignment->coach->id == \Auth::user()->id || \Auth::user()->status == 1)) 
                return true;
-       }
     }
     
     public function sendclientAlert(User $user , assignment $assignment)
     {
-       /*if($assignment->package()->first()->status == 0)
+       if($assignment->package()->first()->status == 0)
             $clientReply=true;
         else
-            $clientReply=false;*/
+            $clientReply=false;
 
-       if((\Auth::user()->id == $assignment->user->id ) ) //$clientReply && 
+       if($clientReply && (\Auth::user()->id == $assignment->user->id ) ) //$clientReply && 
            return true;
     }
 }
