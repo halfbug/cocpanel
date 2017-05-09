500effe798d8d5b7fd00ff893d8cee9a82140486
Sadaf Siddiqui <sadaf_cu@hotmail.com>
2017-05-09 09:33:00 +0500

    one module pack save and continue fixed

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -10,24 +10,22 @@ class AssignmentController extends Controller {
     public function show(Request $request, $assignment_id) {
         $role = $request->session()->get('role');
         $assignment = \App\assignment::find($assignment_id);
-                        
-        if(\Auth::user()->isClient()){
+
+        if (\Auth::user()->isClient()) {
 //            $role_id = \App\role::client();
             $client_id = \Auth::user()->id;
             $coach_id = \App\assignment::find($assignment->coache_id)->user_id;
-            
         } else {
 //            $role_id = \App\role::coache();
-            $client_id=$assignment->user_id;
+            $client_id = $assignment->user_id;
             $coach_id = \Auth::user()->id;
-            
         }
 
         session(['client' => \App\User::find($client_id)]);
         session(['coach' => \App\User::find($coach_id)]);
 
-        
-        
+
+
         $module = \App\module::find($assignment->module_id);
         return view('module.preview')->with('module', $module)->with('assignment', $assignment);
 //        return $package_id."/".$module_id;
@@ -53,17 +51,16 @@ class AssignmentController extends Controller {
 
         $assignment = $discussion->assignment()->first();
 //        replyoff
-        if($assignment->package()->first()->status == 0)
-            $clientReply=true;
-        else
-        {
-            $clientReply=false;
+        if ($assignment->package()->first()->status == 0)
+            $clientReply = true;
+        else {
+            $clientReply = false;
             $discussion->visibility = 1;
             $discussion->save();
         }
         // email to client on coach response
         $user = $discussion->user()->first();
-        session(['assignment_id'=>$assignment->id]);
+        session(['assignment_id' => $assignment->id]);
 //        if(\Auth::user()->isCoach() || \Auth::user()->isAdmin())
 //            {
 //            if($clientReply)
@@ -78,58 +75,63 @@ class AssignmentController extends Controller {
 
         return back()->with('package_id', $package_id)->with('module_id', $module_id);
     }
-    
-    
+
     public function updateStatus(Request $request) {
         echo $request->assignment_id;
-        if($request->assignment_id == 0)
-        {
-            $assign =\App\assignment::create([
-                'role_id'=>\App\role::client(),
-                'user_id'=>$request->user_id,
-                'package_id'=>$request->package_id,
-                'module_id'=>$request->module_id,
-                'status'=>$request->status
-                ]);
-        }
-        else{
-            $assign=\App\assignment::find($request->assignment_id);
+        if ($request->assignment_id == 0) {
+            $assign = \App\assignment::create([
+                        'role_id' => \App\role::client(),
+                        'user_id' => $request->user_id,
+                        'package_id' => $request->package_id,
+                        'module_id' => $request->module_id,
+                        'status' => $request->status
+            ]);
+        } else {
+            $assign = \App\assignment::find($request->assignment_id);
             $assign->status = $request->status;
             $assign->save();
         }
-        
+
         return back();
     }
 
-     public function sendtoclient($assigned_id) {
+    public function sendtoclient($assigned_id) {
         $assignment = \App\assignment::find($assigned_id);
-         \Mail::to($assignment->user->email)->send(new NewResponse(\Auth::user(), 'coach', $assignment->module()->first(),$assignment));
-     
-         return back();    
-     }
-     public function sendtocoach($assigned_id) {
-         $assignment = \App\assignment::find($assigned_id);
-         \Mail::to($assignment->coach->email)->send(new NewResponse(\Auth::user(), 'client',$assignment->module()->first(),$assignment));
-         
-          return back();   
-     }
-     public function savecontinue($assigned_id) {
+        \Mail::to($assignment->user->email)->send(new NewResponse(\Auth::user(), 'coach', $assignment->module()->first(), $assignment));
+
+        return back();
+    }
+
+    public function sendtocoach($assigned_id) {
         $assignment = \App\assignment::find($assigned_id);
-        $next_module= $assignment->package()->first()->modules()->where('modules.id','!=' , $assignment->module_id)->first();
-        if(\App\assignment::where('package_id',$assignment->package_id)->where('module_id',$next_module->id)->where('user_id',$assignment->user_id)->count() > 0)
-        {
-            $assigned_module = \App\assignment::where('package_id',$assignment->package_id)->where('module_id',$next_module->id)->where('user_id',$assignment->user_id)->first();
-        }
-        else{
-             $assigned_module =\App\assignment::create([
-                'role_id'=>\App\role::client(),
-                'user_id'=>$assignment->user_id,
-                'package_id'=>$assignment->package_id,
-                'module_id'=>$next_module->id,
-                'coache_id'=>$assignment->coache_id
+        \Mail::to($assignment->coach->email)->send(new NewResponse(\Auth::user(), 'client', $assignment->module()->first(), $assignment));
+
+        return back();
+    }
+
+    public function savecontinue($assigned_id) {
+        $assignment = \App\assignment::find($assigned_id);
+        $next_module = $assignment->package()->first()->modules()->where('modules.id', '!=', $assignment->module_id)->first();
+        if ($next_module) {
+            $acount = \App\assignment::where('package_id', $assignment->package_id)->where('module_id', $next_module->id)->where('user_id', $assignment->user_id)->count();
+            if ($acount > 0) {
+                $assigned_module = \App\assignment::where('package_id', $assignment->package_id)->where('module_id', $next_module->id)->where('user_id', $assignment->user_id)->first();
+            } elseif ($acount > 1) {
+                $assigned_module = \App\assignment::create([
+                            'role_id' => \App\role::client(),
+                            'user_id' => $assignment->user_id,
+                            'package_id' => $assignment->package_id,
+                            'module_id' => $next_module->id,
+                            'coache_id' => $assignment->coache_id
                 ]);
+            } else {
+                $assigned_module = $assignment;
+            }
+
+            return redirect('/assigned/' . $assigned_module->id);
         }
-        
-         return redirect('/assigned/'.$assigned_module->id);    
-     }
+        else
+            return back()->with('success',"There is only one module in package.");
+    }
+
 }
