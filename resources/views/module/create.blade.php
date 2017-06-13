c87877b1961dd6f54e0108e20aac332010e6a596
Sadaf Siddiqui <sadaf_cu@hotmail.com>
2017-06-13 05:42:24 +0500

    media upload feature DONE
    1. please make "files" folder in public folder. and give it full
    permissions

--- resources/views/module/create.blade.php
+++ resources/views/module/create.blade.php
@@ -252,8 +252,50 @@ tinymce.init({
         'insertdatetime media table contextmenu paste code',
         'textcolor'
     ],
-    toolbar: 'undo redo | insert | styleselect | fontsizeselect | bold italic | forecolor | backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent ',
+    toolbar: 'undo redo | insert | styleselect | fontsizeselect | bold italic | forecolor | backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | media | image ',
   //  content_css: '//www.tinymce.com/css/codepen.min.css'
+  // enable title field in the Image dialog
+  image_title: true, 
+  // enable automatic uploads of images represented by blob or data URIs
+  automatic_uploads: true,
+  // URL of our upload handler (for more details check: https://www.tinymce.com/docs/configure/file-image-upload/#images_upload_url)
+  images_upload_url: 'http://localhost:82/laravel5.3/bulleye/cocpanel/public/postAcceptor.php',
+  // here we add custom filepicker only to Image dialog
+  file_picker_types: 'image | media | files', 
+  images_upload_base_path: '../',
+  // and here's our custom image picker
+  file_picker_callback: function(cb, value, meta) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'files/*');
+    
+    // Note: In modern browsers input[type="file"] is functional without 
+    // even adding it to the DOM, but that might not be the case in some older
+    // or quirky browsers like IE, so you might want to add it to the DOM
+    // just in case, and visually hide it. And do not forget do remove it
+    // once you do not need it anymore.
+
+    input.onchange = function() {
+      var file = this.files[0];
+      
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        // Note: Now we need to register the blob in TinyMCEs image blob
+        // registry. In the next release this part hopefully won't be
+        // necessary, as we are looking to handle it internally.
+        var id = 'blobid' + (new Date()).getTime();
+        var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+        var blobInfo = blobCache.create(id, file, reader.result);
+        blobCache.add(blobInfo);
+
+        // call the callback and populate the Title field with the file name
+        cb(blobInfo.blobUri(), { title: file.name });
+      };
+    };
+    
+    input.click();
+  }
 });
 
 tinymce.init({
@@ -268,6 +310,48 @@ tinymce.init({
     ],
     toolbar: 'undo redo | insert | styleselect | fontsizeselect | bold italic | forecolor | backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent ',
    // content_css: '//www.tinymce.com/css/codepen.min.css'
+   // enable title field in the Image dialog
+  image_title: true, 
+  // enable automatic uploads of images represented by blob or data URIs
+  automatic_uploads: true,
+  // URL of our upload handler (for more details check: https://www.tinymce.com/docs/configure/file-image-upload/#images_upload_url)
+  images_upload_url: 'http://localhost:82/laravel5.3/bulleye/cocpanel/public/postAcceptor.php',
+  // here we add custom filepicker only to Image dialog
+  file_picker_types: 'image | media | files', 
+  images_upload_base_path: '../',
+  // and here's our custom image picker
+  file_picker_callback: function(cb, value, meta) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'files/*');
+    
+    // Note: In modern browsers input[type="file"] is functional without 
+    // even adding it to the DOM, but that might not be the case in some older
+    // or quirky browsers like IE, so you might want to add it to the DOM
+    // just in case, and visually hide it. And do not forget do remove it
+    // once you do not need it anymore.
+
+    input.onchange = function() {
+      var file = this.files[0];
+      
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        // Note: Now we need to register the blob in TinyMCEs image blob
+        // registry. In the next release this part hopefully won't be
+        // necessary, as we are looking to handle it internally.
+        var id = 'blobid' + (new Date()).getTime();
+        var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+        var blobInfo = blobCache.create(id, file, reader.result);
+        blobCache.add(blobInfo);
+
+        // call the callback and populate the Title field with the file name
+        cb(blobInfo.blobUri(), { title: file.name });
+      };
+    };
+    
+    input.click();
+  }
 });
 </script>
 @endsection
