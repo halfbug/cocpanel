57c8fd05d5f3570a2ca700adf069f93eeed19d2c
Sadaf Siddiqui <sadaf_cu@hotmail.com>
2017-06-13 06:01:13 +0500

    -remove

--- resources/views/module/create.blade.php
+++ resources/views/module/create.blade.php
@@ -254,6 +254,7 @@ tinymce.init({
     ],
     toolbar: 'undo redo | insert | styleselect | fontsizeselect | bold italic | forecolor | backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | media | image ',
   //  content_css: '//www.tinymce.com/css/codepen.min.css'
+
   // enable title field in the Image dialog
   image_title: true, 
   // enable automatic uploads of images represented by blob or data URIs
@@ -295,7 +296,12 @@ tinymce.init({
     };
     
     input.click();
-  }
+  },
+
+    content_css : "../css/custom_content.css",
+    theme_advanced_font_sizes: "10px,12px,13px,14px,16px,18px,20px",
+    font_size_style_values : "10px,12px,13px,14px,16px,18px,20px"
+
 });
 
 tinymce.init({
@@ -309,6 +315,7 @@ tinymce.init({
         'textcolor'
     ],
     toolbar: 'undo redo | insert | styleselect | fontsizeselect | bold italic | forecolor | backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent ',
+
    // content_css: '//www.tinymce.com/css/codepen.min.css'
    // enable title field in the Image dialog
   image_title: true, 
@@ -351,7 +358,12 @@ tinymce.init({
     };
     
     input.click();
-  }
+  },
+    //content_css: '//www.tinymce.com/css/codepen.min.css'
+    content_css : "../css/custom_content.css",
+    theme_advanced_font_sizes: "10px,12px,13px,14px,16px,18px,20px",
+    font_size_style_values : "10px,12px,13px,14px,16px,18px,20px"
+
 });
 </script>
 @endsection
